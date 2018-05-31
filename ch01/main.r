c5869f58a96622c2fd4af7572c6080be23d58c17
Vagacoder <30400336+Vagacoder@users.noreply.github.com>
2018-05-30 19:08:42 -0700

    Add files via upload

---
+++ ch01/main.r
@@ -0,0 +1,15 @@
+# ch01
+# my first program in R programming
+
+mystring <- "Hello world!"
+print(mystring)
+mean(1:5)
+sd(0:100)
+apropos("string")
+apropos("z$")
+example(plot)
+demo()
+demo(plotmath)
+demo(Japanese)
+browseVignettes()
+RSiteSearch("{Bayesian regression}")

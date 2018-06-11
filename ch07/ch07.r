3d581945d323e6def05319a0f07a55179dcf445c
Qirui Hu <qhu@sbcc.local>
2018-06-11 12:36:13 -0700

    add files

--- ch07/ch07.r
+++ ch07/ch07.r
@@ -72,7 +72,7 @@ alarm()
 
 # change case
 toupper("I'm shouting")
-tolower("I'm shouting")
+tolower("I'm SHOuting")
 
 # substring
 woodchuck <- c(
@@ -138,3 +138,124 @@ factor(gender_fac, levels = c("male", "female"))
 levels(gender_fac) <- c("male", "female")
 gender_fac
 relevel(gender_fac, "male")
+
+# drop factor levels
+getting_to_work <- data.frame(
+  mode = c(
+    "bike", "car", "bus", "car", "walk",
+    "bike", "car", "bike", "car", "car"
+  ),
+  time_mins = c(25, 13, NA, 22, 65, 28, 15, 24, NA, 14)
+)
+getting_to_work
+(getting_to_work <- subset(getting_to_work, !is.na(time_mins)))
+subset(getting_to_work, is.na(time_mins))
+
+# unique function
+unique(getting_to_work$mode)
+
+# droplevels()
+getting_to_work$mode <- droplevels(getting_to_work$mode)
+getting_to_work
+getting_to_work <- droplevels(getting_to_work)
+levels(getting_to_work$mode)
+
+# ordered factors
+happy_choices <- c("depressed", "grumpy", "so-so", "cheery", "ecstatic")
+happy_values <- sample(happy_choices, 10000, replace =  TRUE)
+happy_choices
+happy_values
+happy_fac <- factor(happy_values, happy_choices)
+head(happy_fac)
+
+happy_ord <- ordered(happy_values, happy_choices)
+head(happy_ord)
+
+is.factor(happy_ord)
+is.ordered(happy_ord)
+is.ordered(happy_fac)
+
+# Converting variale, between continous and categorical
+ages <- 16 + 50 * rbeta(10000, 2, 3)
+ages
+grouped_ages <- cut(ages, seq.int(16, 66, 10))
+grouped_ages
+head(grouped_ages)
+table(grouped_ages)
+# test cut()
+cut(c(11, 22, 23,34,55,62), seq.int(10,70, 10))
+class(cut(c(11, 22, 23,34,55,62), seq.int(10,70, 10)))
+class(ages)
+class(grouped_ages)
+# make a histogram 
+hist(ages)
+
+# processing dirty data
+dirty <- data.frame(x = c("1.23","4..56", "7.89"))
+dirty
+as.numeric(dirty$x)
+
+as.character(dirty$x)
+as.numeric(as.character(dirty$x))
+
+as.numeric(levels(dirty$x))
+as.numeric(levels(dirty$x))[as.integer(dirty$x)]
+
+# generat factor levels
+gl(3,2)
+gl(3,2, labels = c("placebo", "drug A", "drug B"))
+gl(3, 1, 6, labels = c("placebo", "drug A", "drug B"))
+
+# combine factors
+treatment <- gl(3, 2, labels = c("placebo", "drug A", "drug B"))
+gender <- gl(2, 1, 6, labels = c("female", "male"))
+interaction(treatment, gender)
+
+## Quiz
+# Q7-1
+format()
+formatC()
+sprintf()
+prettyNum()
+
+# Q7-2
+alarm()
+print("\a")
+
+# Q7-3
+# factor and ordered factors
+
+# Q7-4
+# get NA
+
+# Q7-5
+cut()
+
+## Exercises
+# E7-1
+sprintf("%.15f", pi)
+formatC(pi, digits = 16)
+
+# E7-2
+x <- c(
+  "Swan swam over the pond, Swim swan swim!",
+  "Swan swam back again - Well swum swan!"
+)
+strsplit(x, ",? -? ?")
+
+# E7-3
+three_d6 <- function(n)
+{
+  random_numbers <- matrix(
+    sample(6, 3 * n, replace = TRUE),
+    nrow = 3
+  )
+  colSums(random_numbers)
+}
+rt <- c(three_d6(1000))
+rt
+unique(rt)
+table(rt)
+t <-cut(rt, c(2,3,5,8,12,15,17,18))
+t <-cut(rt, c(2,3,5,8,12,15,17,18), labels = -3:3)
+table(t)      
---
+++ ch08/ch08.r
@@ -0,0 +1,7 @@
+## Chpater 08
+# Flow controls and loops
+
+# if and else
+if(TRUE) message("It was true!")
+if(FALSE) message("It wasN'T true!")
+if(NA) message("Missing something")

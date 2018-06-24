36d0b71122432403e578c6e50a01c299f0764264
Vagacoder <qiruihu@gmail.com>
2018-06-24 11:50:46 -0700

    update a file

--- ch13/ch13.r
+++ ch13/ch13.r
@@ -123,7 +123,7 @@ na.fail(deer_endocranial_volume)
 deer_wide <- deer_endocranial_volume[, 1:5]
 deer_wide
 
-# reshape2 pkg
+# melt function in reshape2 pkg (reshape2 pkg is better than reshape pkg)
 library(reshape2)
 deer_long <- melt(deer_wide, id.vars = "SkullID")
 head(deer_long)
@@ -134,5 +134,152 @@ melt(deer_wide, measure.vars = c("VolCT", "VolBead", "VolLWH", "VolFinarelli"))
 # dcast function from long to wide
 deer_wide_again <- dcast(deer_long, SkullID ~ variable)
 deer_wide_again
-# notice that data frame is sorted by SkullID
+class(deer_wide_again)
+# notice that data frame from dcast function above, is sorted by SkullID
 deer_endocranial_volume
+
+# acast return a vector/matrix/array
+deer_wide_again1 <- acast(deer_long, SkullID ~ variable)
+deer_wide_again1
+class(deer_wide_again1)
+
+# using SQL
+# R functions are more concise and readable than SQL
+install.packages("sqldf")
+library(RSQLite)
+library(DBI)
+library(sqldf)
+
+# get subset from SQL file directly
+subset(deer_endocranial_volume,
+  VolCT > 400 | VolCT2 > 400,
+  c(VolCT, VolCT2)
+)
+
+query <-  "SELECT VolCT, VolCT2 
+FROM 
+  deer_endocranial_volume 
+WHERE
+  VolCT > 400 OR
+  VolCT2 > 400"
+sqldf(query)
+
+# 3. Sorting
+x <- c(2, 32, 4, 16, 8)
+sort(x)
+sort(x, decreasing = TRUE)
+sort(c("I", "shot", "the", "city", "sheriff"))
+order(x)
+
+### sort(x) = x[order(x)]
+x[order(x)]
+
+## order() is useful for data frame, 
+# while sort() cannot be directly used in data frame
+year_order <- order(english_monarchs$start.of.reign)
+year_order
+english_monarchs[year_order, ]
+
+# arrange function form plyr pkg
+library(plyr)
+arrange(english_monarchs, start.of.reign)
+
+# rank function
+(x <- sample(3, 7, replace = TRUE))
+rank(x)
+rank(x, ties.method = "first")
+
+# 4. Functional Programming
+# Negate() returns a reversed boolean value
+ct2 <- deer_endocranial_volume$VolCT2
+# note the argument is.na is a function ( is.na() )
+isnt.na <- Negate(is.na)
+identical(isnt.na(ct2), !is.na(ct2))
+
+# filter()
+Filter(isnt.na, ct2)
+
+# position()
+Position(isnt.na, ct2)
+
+# Map()
+get_volume <- function(ct, bead, lwh, finarelli, ct2, bead2, lwh2){
+#If there is a second measurement, take the average
+if(!is.na(ct2)){
+ct <- (ct + ct2) / 2
+bead <- (bead + bead2) / 2
+lwh <- (lwh + lwh2) / 2
+}
+#Divide lwh by 4 to bring it in line with other measurements
+c(ct = ct, bead = bead, lwh.4 = lwh / 4, finarelli = finarelli)
+}
+
+measurements_by_deer <- with(
+deer_endocranial_volume,
+Map(
+get_volume,
+VolCT,
+VolBead,
+VolLWH,
+VolFinarelli,
+VolCT2,
+VolBead2,
+VolLWH2
+)
+)
+head(measurements_by_deer)
+
+# reduce()
+Reduce("+", list(1, 2, 3, 4, 5))
+pmax2 <- function(x, y) ifelse(x >= y, x, y)
+Reduce(pmax2, measurements_by_deer)
+
+## Quiz
+# Q13-1
+# step 1 file object
+shake <- system.file("extdata", "Shakespeare.s.The.Tempest..from.Project.Gutenberg.pg2235.txt", package = "learningr")
+shake
+# step 2 read the file to lines
+shake_lines <- readLines(shake)
+shake_lines
+# step 3 split lines to words
+shake_words <- strsplit(shake_lines, ", | ")
+shake_words
+# step 4 count the number of "thou"
+sum(str_count(shake_words, "thou"))
+
+# Q13-2
+# with(), within(), transform() and mutate()
+
+# Q13-3
+# dcast() and acast()
+
+# Q13-4
+# sort( dataframe$colname)
+# order(x) first, then x[order(x)]
+# arrange()
+
+# Q13-5
+# position(x > 0)
+# ispositive <- functin(x) x >0
+# Find(ispositive, x)
+# position(ispositive, x)
+
+## Exercises
+# E13-1
+# 1
+hafuFile <- system.file("extdata", "hafu.csv", package = "learningr")
+hafuData <- read.csv(hafuFile)
+head(hafuData)
+hafu_father_uncertain <- str_detect(hafuData$Father, fixed("?"))
+head(hafu_father_uncertain)
+hafu_mother_uncertain <- str_detect(hafuData$Mother, fixed("?"))                                
+head(hafu_mother_uncertain)
+hafuData$Uncertain_Father <- hafu_father_uncertain
+hafuData$Uncertain_Mother <- hafu_mother_uncertain
+head(hafuData)
+
+# 2 
+hafuData$Father <- str_replace(hafuData$Father, fixed("?"), "")
+hafuData$Mother <- str_replace(hafuData$Mother, fixed("?"), "")
+head(hafuData)

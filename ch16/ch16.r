8bb0c1c7e04c5f44374b2d1d5202f40c9a0b09b4
Vagacoder <qiruihu@gmail.com>
2018-07-05 11:47:04 -0700

    update a file

--- ch16/ch16.r
+++ ch16/ch16.r
@@ -165,5 +165,174 @@ test_dir <- system.file("tests", package = "learningr")
 suite <- defineTestSuite("hypotenuse suite", test_dir)
 runTestSuite(suite)
 
+# testthat pkg
+install.packages("testthat")
+library(testthat)
+# expect_*
+expect_equal(hypotenuse(3, 4), 5)
+expect_error(hypotenuse())
+expect_equal(hypotenuse(1e-300, 1e-300), sqrt(2) * 1e-300, tol = 1e-305)
+expect_equal(hypotenuse(1e300, 1e300), sqrt(2) * 1e300)
+
+# test_file finction
+filename <- system.file(
+  "tests",
+  "testthat_hypotenuse_tests.R",
+  package = "learningr"
+)
+test_file(filename)
+
+# test_that() and test_package
+test_that()
+
+# warning be tested via expect_warning
+expect_warning(log(0))
+
+# Magic
+# turn the string into code
+# e.g.
+atan(c(-Inf, -1, 0, 1, Inf))
+# quote function and eval function
+# R do eval(quote())
+(quoted_r_code <- quote(atan(c(-Inf, -1, 0, 1, Inf))))
+class(quoted_r_code)
+eval(quoted_r_code)
+# check the call object
+as.list(quoted_r_code)
+
+# In R, everything is a function
+vapply(  list("+", "if", "for", "<-", "[","[["),
+  is.function, logical(1))
+is.function('for')
+
+# parse function
+parse(filename)
+parsed_r_code <- parse(text = "atan(c(-Inf, -1, 0, 1, Inf))")
+class(parsed_r_code)
+eval(parsed_r_code)
+
+# turn code into string
+# e.g. 
+random_numbers <- rt(1000, 2)
+hist(random_numbers)
+# substitue function and deparse function
+divider <- function(numerator, denominator){
+  if(denominator == 0)  {
+    denominator_name <- deparse(substitute(denominator))
+    warning("The denominator, ", sQuote(denominator_name), ", is zero.")
+  }
+  numerator / denominator
+}
+top <- 3
+bottom <- 0
+divider(top, bottom)
+
+# 5, OOP
+# s3 class
+print
+today <- Sys.Date()
+print(today)
+print.Date
+head(methods(print))
 
 
+# Reference Class
+my_class_generator <- setRefClass(
+  "MyClass",
+  fields = list(
+    #data variables are defined here
+  ),
+  methods = list(
+    #functions to operate on that data go here
+    initialize = function(...)
+    {
+      #initialize is a special function called
+      #when an object is created.
+    }
+  )
+)
+
+point_generator <- setRefClass(
+  "point",
+  fields = list(
+    x = "numeric",
+    y = "numeric"
+  ),
+  methods = list(
+    initialize = function(x = NA_real_, y = NA_real_)
+    {
+      "Assign x and y upon object creation."
+      x <<- x
+      y <<- y
+    },
+    distanceFromOrigin = function()
+    {
+      "Euclidean distance from the origin"
+      sqrt(x ^ 2 + y ^ 2)
+    },
+    add = function(point)
+    {
+      "Add another point to this point"
+      x <<- x + point$x
+      y <<- y + point$y
+      .self
+    }
+  )
+)
+
+(a_point <- point_generator$new(5, 3))
+
+# construct a new object
+point_generator(1, 10)
+point_generator$help("initialize")
+
+create_point <- function(x, y){
+  point_generator$new(x, y)
+}
+a_point <- create_point(3,4)
+a_point$distanceFromOrigin()
+a_point$add(a_point)
+# list the fields and methods in class
+point_generator$fields()
+point_generator$methods()
+
+# inheritance
+three_d_point_generator <- setRefClass(
+  "three_d_point",
+  fields = list(
+    z = "numeric"
+  ),
+  contains = "point", #this line lets us inherit
+  methods = list(
+    initialize = function(x, y, z)
+    {
+      "Assign x and y upon object creation."
+      x <<- x
+      y <<- y
+      z <<- z
+    },
+    distanceFromOrigin = function()
+    {
+      "Euclidean distance from the origin"
+      sqrt(x ^ 2 + y ^ 2 + z ^ 2)
+    }
+  )
+)
+a_three_d_point <- three_d_point_generator$new(3, 4, 5)
+a_three_d_point$distanceFromOrigin()
+
+## Quiz
+# Q16-1
+# warnings()
+
+# Q16-2
+# return an object of try-error
+
+# Q16-3
+# checkException in RUnit = expect_* in testthat
+
+# Q16-4
+# quote and eval functions
+
+# Q16-5
+# overload methods
